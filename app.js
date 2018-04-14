cf0732c181fe69e6baafe0127ea7469270a52f9d
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-14 16:14:23 -0400

    update idea in DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 //initialize application
 const app = express();
@@ -30,6 +31,9 @@ app.set('view engine', 'handlebars');
 //Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Method override middleware
+app.use(methodOverride('_method'))
 
 //Index Route
 app.get('/', (req, res) => {
@@ -105,6 +109,23 @@ app.post('/ideas', (req, res) => {
         res.redirect('/ideas');
       })
   }
+})
+
+//edit form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    //update idea
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+
+    idea.save()
+    .then(idea => {
+      res.redirect('/ideas');
+    })
+  })
 })
 
 //port for app to listen
