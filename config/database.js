16532a33ed14f45dc645acc1578cb38548e4c514
Kumar Rohit Malhotra <krohitm@gmail.com>
2018-04-22 16:03:14 -0400

    securing db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: 'mongodb://krohitm:krohitm@ds119772.mlab.com:19772/vidjot-prod' }
+  module.exports = { mongoURI: 'mongodb://krohitm:vidjotkrm123@ds119772.mlab.com:19772/vidjot-prod' }
 }
 else {
   module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' }
